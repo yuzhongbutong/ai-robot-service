59a48510112e636d01c12d08ada778f851c29997
zhao jingyu <dlzhaojy@cn.ibm.com>
2018-12-18 21:44:45 +0800

    fixed not work in Edge.

--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dcf9849722a2eca978ec81d621affe14.js"
+  "/precache-manifest.c2897e103e42fb698c8fe0780696ab03.js"
 );
 
 workbox.clientsClaim();
